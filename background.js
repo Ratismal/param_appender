7a2e80b3c43ea960bee4f633efd5c2d3907f6393
stupid cat <cat@blargbot.xyz>
2019-06-28 12:51:43 -0600

    replace state instead of push

--- background.js
+++ background.js
@@ -1,17 +1,17 @@
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.url) {
         chrome.storage.sync.get({
             parameter: 'param=true',
             whitelist: '.\ngoogle\\.com'
         }, items => {
-            if(changeInfo.url.indexOf(items.parameter) === -1 && /^https?/.test(changeInfo.url) 
+            if (changeInfo.url.indexOf(items.parameter) === -1 && /^https?/.test(changeInfo.url)
                 && items.whitelist.trim().split('\n').map(wl => new RegExp(wl, 'i').test(changeInfo.url)).reduce((acu, cur) => acu || cur, false)) {
                 let url = changeInfo.url;
                 if (url.indexOf('?') > -1) url += '&';
                 else url += '?';
                 url += items.parameter;
                 chrome.tabs.executeScript(tab.id, {
-                    code: `window.history.pushState('param appender', 'param appended', '${url}')`
+                    code: `window.history.replaceState('param appender', 'param appended', '${url}')`
                 });
             }
         });
