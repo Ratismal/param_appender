4fb0541aadd99457cd90881bc4bd276c58fd8b3d
Kirkland Morrill <cat@blargbot.xyz>
2020-03-04 12:33:36 -0700

    support multiple parameters

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,6 +1,8 @@
 let config = {
-    parameter: 'param=true',
-    whitelist: '.\ngoogle\\.com'
+    items: [{
+        parameter: 'param=true',
+        whitelist: '.\ngoogle\\.com'
+    }]
 };
 
 chrome.storage.sync.get(config, items => {
@@ -15,15 +17,16 @@ chrome.storage.sync.onChanged.addListener(changes => {
 chrome.webRequest.onBeforeRequest.addListener(
     function (details) {
         if (details.url) {
-            console.log(details, config);
-            if (details.url.indexOf(config.parameter) === -1 && /^https?/.test(details.url)
-                && config.whitelist.trim().split('\n').map(wl => new RegExp(wl, 'i').test(details.url)).reduce((acu, cur) => acu || cur, false)) {
-                let url = details.url;
-                if (url.indexOf('?') > -1) url += '&';
-                else url += '?';
-                url += config.parameter;
-                console.log('redirecting to', url);
-                return { redirectUrl: url };
+            for (const item of config.items) {
+                if (details.url.indexOf(item.parameter) === -1 && /^https?/.test(details.url)
+                    && item.whitelist.trim().split('\n').map(wl => new RegExp(wl, 'i').test(details.url)).reduce((acu, cur) => acu || cur, false)) {
+                    let url = details.url;
+                    if (url.indexOf('?') > -1) url += '&';
+                    else url += '?';
+                    url += item.parameter;
+                    console.log('redirecting to', url);
+                    return { redirectUrl: url };
+                }
             }
         }
     },
--- src/scripts/options.js
+++ src/scripts/options.js
@@ -1,21 +1,66 @@
+function storageGet(opts) {
+  return new Promise(res => {
+    chrome.storage.sync.get(opts, res);
+  })
+}
+function storageSet(opts) {
+  return new Promise(res => {
+    chrome.storage.sync.set(opts, res);
+  })
+}
+function sleep(time = 1000) {
+  return new Promise(res => setTimeout(res, time));
+}
+function defaultParam() {
+  return {
+    parameter: 'param=true',
+    whitelist: '.\ngoogle\\.com'
+  };
+}
+
 const app = new Vue({
   el: '#app',
   data() {
     return {
-      whitelist: '',
-      whitelistList: [],
-      param: '',
+      items: [],
       status: '',
       inputType: 'text',
-      inputTypes: ['text', 'list']
+      inputTypes: ['text', 'list'],
+      selected: 0
     }
   },
   computed: {
+    selectedParam() {
+      return this.items[this.selected];
+    },
+    warnings() {
+      let param = this.selectedParam;
+      let warnings = [];
+      if (param && param.whitelist) {
+        let whitelist = param.whitelist.replace(/\r/g, '').split('\n');
+        if (whitelist.includes('')) {
+          warnings.push('- Whitelist includes empty lines. This means it\'ll match anything.');
+        }
+      }
+      return warnings;
+    }
   },
   mounted() {
     this.restore();
   },
   methods: {
+    setParam(i) {
+      this.selected = i;
+    },
+    newParam() {
+      this.items.push(defaultParam());
+      this.selected = this.items.length - 1;
+    },
+    deleteParam(i) {
+      this.items.splice(i, 1);
+      this.selected--;
+      if (this.selected < 0) this.selected = 0;
+    },
     setInputType(type) {
       if (type !== this.inputType) {
         this.inputType = type;
@@ -34,31 +79,46 @@ const app = new Vue({
         }
       }
     },
-    save() {
+    async save() {
       if (this.inputType === 'list') {
         this.whitelist = this.whitelistList.join('\n');
       }
-      chrome.storage.sync.set({
-        parameter: this.param,
-        whitelist: this.whitelist
-      }, () => {
-        // Update status to let user know options were saved.
-        this.status = 'Options saved.';
-        setTimeout(() => {
-          this.status = '';
-        }, 750);
+      // const items = [...this.items].map(item => ({
+      //   parameter: item.parameter,
+      //   whitelist: item.whitelist.join('\n'),
+      // }));
+
+      await storageSet({
+        items: this.items.map(item => ({
+          parameter: item.parameter,
+          whitelist: item.whitelist.replace(/\r/g, '')
+        }))
       });
+
+      this.status = 'Options saved.';
+      await sleep(750);
+      this.status = '';
     },
-    restore() {
+    async restore() {
       // Use default value color = 'red' and likesColor = true.
-      chrome.storage.sync.get({
+      const config = await storageGet({
         parameter: 'param=true',
-        whitelist: '.\ngoogle\\.com'
-      }, (items) => {
-        this.param = items.parameter;
-        this.whitelist = items.whitelist;
-        this.whitelistList = items.whitelist.split('\n');
+        whitelist: '.\ngoogle\\.com',
+        items: [defaultParam()]
       });
+      if (config.parameter && config.whitelist) {
+        config.items[0].parameter = config.parameter;
+        config.items[0].whitelist = config.whitelist;
+        await storageSet({
+          parameter: null,
+          whitelist: null,
+          items: config.items
+        });
+      }
+      this.items = config.items;
+      // for (const item of this.items) {
+      //   item.whitelist = item.whitelist.split('\n');
+      // }
     },
     removeLine(i) {
       this.whitelistList.splice(i, 1);
